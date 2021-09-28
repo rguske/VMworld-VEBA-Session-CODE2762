98c96eb1e2ccb0314a988ada4bbe069e0b0f3df6
Robert Guske <rguske@vmware.com>
2021-09-28 14:02:57 +0200

    scripts improvements were made
    Signed-off-by: Robert Guske <rguske@vmware.com>

--- knative_vmware_event_router_install.sh
+++ knative_vmware_event_router_install.sh
@@ -9,62 +9,80 @@
 #
 # see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
 #
-DEMO_PROMPT="${GREEN}➜ ${CYAN}\W "
+DEMO_PROMPT="${GREEN}➜ ${GREEN}\W "
 
 # hide the evidence
 clear
 
-# show kubernetes cluster nodes
-pei "kubectl get nodes -o wide"
+# # show kubernetes cluster nodes
+# pei "kubectl get nodes -o wide"
+
+# print out kn serving
+pei "figlet Knative Serving  | lolcat"
 
 # export knative serving and kn-contour release versions
 pei "export KN_SERVING='v0.26.0'"
 pei "export KN_CONTOUR='v0.26.0'"
 
 # install knative serving crds and core
-pe "kubectl apply -f https://github.com/knative/serving/releases/download/$KN_SERVING/serving-crds.yaml && kubectl apply -f https://github.com/knative/serving/releases/download/$KN_SERVING/serving-core.yaml && kubectl wait deployment --all --timeout=-1s --for=condition=Available -n knative-serving"
+pe "kubectl apply -f https://github.com/knative/serving/releases/download/$KN_SERVING/serving-crds.yaml \
+    && kubectl apply -f https://github.com/knative/serving/releases/download/$KN_SERVING/serving-core.yaml \
+    && kubectl wait deployment --all --timeout=-1s --for=condition=Available -n knative-serving"
 
 # hide the evidence
 pe clear
 
 # install contour ingress controller and patch configmap config-network
 pe "kubectl apply -f https://github.com/knative/net-contour/releases/download/$KN_CONTOUR/release.yaml"
-pei "kubectl patch configmap/config-network --namespace knative-serving --type merge --patch '{\"data\":{\"ingress.class\":\"contour.ingress.networking.knative.dev\"}}' && kubectl wait deployment --all --timeout=-1s --for=condition=Available -n contour-external && kubectl wait deployment --all --timeout=-1s --for=condition=Available -n contour-internal"
+pe "kubectl patch configmap/config-network --namespace knative-serving --type merge --patch '{\"data\":{\"ingress.class\":\"contour.ingress.networking.knative.dev\"}}' \
+    && kubectl wait deployment --all --timeout=-1s --for=condition=Available -n contour-external \
+    && kubectl wait deployment --all --timeout=-1s --for=condition=Available -n contour-internal"
 
 # hide the evidence
 pe clear
 
 # export necessary variables for the config-domain configmap patch
+pe "kubectl get service envoy -n contour-external --output 'jsonpath={.status.loadBalancer.ingress[0].ip}'"
 pei "export EXTERNAL_IP=$(kubectl get service envoy -n contour-external --output 'jsonpath={.status.loadBalancer.ingress[0].ip}')"
-pei "export KNATIVE_DOMAIN=$EXTERNAL_IP.vmworld.io"
-pei "dig $KNATIVE_DOMAIN"
+pei "export KNATIVE_DOMAIN=$EXTERNAL_IP.nip.io"
+# pei "dig $KNATIVE_DOMAIN"
 
 # hide the evidence
 pe clear
 
 # patch configmap config-domain
-pei "kubectl patch configmap -n knative-serving config-domain -p '{\"data\": {\"$KNATIVE_DOMAIN\": \"\"}}'"
+pe "kubectl patch configmap -n knative-serving config-domain -p '{\"data\": {\"$KNATIVE_DOMAIN\": \"\"}}'"
 
 # hide the evidence
-clear
+pe clear
+
+# print out kn-eventing
+pei "figlet Knative Eventing  | lolcat"
 
 # export knative eventing release version
 pei "export KN_EVENTING=v0.26.0"
 
 # deploy knative eventing core and crds
-pe "kubectl apply --filename https://github.com/knative/eventing/releases/download/$KN_EVENTING/eventing-crds.yaml && kubectl apply --filename https://github.com/knative/eventing/releases/download/$KN_EVENTING/eventing-core.yaml && kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing"
+pe "kubectl apply --filename https://github.com/knative/eventing/releases/download/$KN_EVENTING/eventing-crds.yaml \
+    && kubectl apply --filename https://github.com/knative/eventing/releases/download/$KN_EVENTING/eventing-core.yaml \
+    && kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing"
 
 # hide the evidence
-clear
+pe clear
 
-# deploy knative in-memory channel
-pe "kubectl apply --filename https://github.com/knative/eventing/releases/download/$KN_EVENTING/in-memory-channel.yaml && kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing"
+# deploy knative in-memory broker
+pe "kubectl apply --filename https://github.com/knative/eventing/releases/download/$KN_EVENTING/in-memory-channel.yaml \
+    && kubectl apply --filename https://github.com/knative/eventing/releases/download/$KN_EVENTING/mt-channel-broker.yaml \
+    && kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing"
 
 # hide the evidence
-clear
+pe clear
 
-# deploy knative mt-channel-broker
-pe "kubectl apply --filename https://github.com/knative/eventing/releases/download/$KN_EVENTING/mt-channel-broker.yaml && kubectl wait pod --timeout=-1s --for=condition=Ready -l '!job-name' -n knative-eventing"
+# create namespace for function
+pe "kubectl create ns vmware-functions"
+
+# hide the evidence
+pe clear
 
 # create a broker in namespace vmware-functions
 pe "kubectl create -f - <<EOF
@@ -84,10 +102,10 @@ spec:
 EOF"
 
 # hide the evidence
-clear
+pe clear
 
-# create namespace for function
-pe "kubectl create ns vmware-functions"
+# print out event-router
+pei "figlet Event-Router  | lolcat"
 
 # create the override.yaml file for the vmware event router deployment
 pe "cat << EOF > override.yaml
@@ -110,16 +128,19 @@ eventrouter:
 EOF"
 
 # hide the evidence
-clear
+pe clear
 
 # helm search; for available vmware-veba versions
-pe "helm search repo vmware-veba"
+pe "helm search repo vmware-veba -l"
 
 # install the vmware event router via helm
-pe "helm install -n vmware-system --create-namespace veba-knative vmware-veba/event-router -f override.yaml --wait --version 0.6.6"
+pe "helm install -n vmware-system --create-namespace veba-knative vmware-veba/event-router -f override.yaml --wait"
 
 # hide the evidence
-clear
+pe clear
+
+# print out function example
+pei "figlet Slack Function  | lolcat"
 
 # create kn-ps-slack function secret
 pe "kubectl -n vmware-functions create secret generic slack-secret --from-file=SLACK_SECRET=slack_secret.json"
@@ -128,13 +149,16 @@ pe "kubectl -n vmware-functions create secret generic slack-secret --from-file=S
 pe "cat function.yaml"
 
 # hide the evidence
-clear
-
-# ensure a VM is running
-p "VM is running?"
+pe clear
 
 # create the kn-ps-slack function
 pe "kubectl -n vmware-functions apply -f function.yaml"
+
+# hide the evidence
+pe clear
+
+# ensure a VM is running
+p "VM is running?"
 
 # stop the running VM
 p "Show function log and powerOff the running VM"
